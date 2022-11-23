20a9498e8ce0d41ddb8d3cebc4f72181c31a9211
Corey Sawin <sawincp@gmail.com>
2022-11-23 12:36:42 -0500

    commit tree aab53c1d1838bdb083df8b09a70f88baf5f6df40

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(event = "roller-skate"){
+    return `This Saturday, I want to ${event}!`
+}
+
+function mondayWork(event = "go to the office"){
+    return `This Monday, I will ${event}.`
+}
+mondayWork("work from home")
+
+function wrapAdjective(flair = "*"){
+    return function (adjective = "Special"){
+        return `You are ${flair}${adjective}${flair}!`;
+    }
+}
+wrapAdjective("a dedicated programmer")("||");
